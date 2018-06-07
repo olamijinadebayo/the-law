35a56ac8f29955139ca77e3ce22f05c0fce65ce8
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-07 20:19:28 +0300

    added landing page to the templates

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ola/Desktop/core-projects/the-law/virtual"
+set -gx VIRTUAL_ENV "/home/wesley/Documents/the-law/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
