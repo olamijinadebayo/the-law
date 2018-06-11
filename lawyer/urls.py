16ce139b02cc480da947e95b43dcce597a4f4192
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-11 22:16:06 +0300

    created necessary templates for lawyer

--- lawyer/urls.py
+++ lawyer/urls.py
@@ -3,7 +3,7 @@ from .import views
 
 urlpatterns = [
     url(r'lawyer/$', views.lawyerdashboard, name= 'lawyerdashboard'),
-    url(r'lawyer/profile/', views.lawyerprofile,name = 'lawyerprofile'),
+    url(r'lawyer/profile/', views.lawyerprofile, name = 'lawyerprofile'),
     url(r'lawyer/cases/', views.lawyercases,name = 'lawyercases'),
 
 ]
