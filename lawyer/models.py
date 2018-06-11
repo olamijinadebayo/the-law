af1dbbae177fa8b8def126815fe013ba7aaf6731
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-11 16:42:42 +0300

    introduced imagekit

--- lawyer/models.py
+++ lawyer/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
-    is_lawyer = models.BooleanField(default=False)
+
+
+class Articles(models.Model):
+    image = ProcessedImageField(upload_to = 'posts/', processors=[ResizeToFill(700,700)], format = 'JPEG', options ={'quality':100})
+    name = models.CharField(max_length=30)
+    post = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
     
