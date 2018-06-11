31ea521def91d9042d68d58e39ded5f7ee573839
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-11 15:44:44 +0300

    new changes

--- law/settings.py
+++ law/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'law.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD':config('DB_PASSWORD'),
+
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
