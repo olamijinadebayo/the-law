bb434f6f08a50cb70e2c582924fed3aa39c1bd82
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-11 23:25:28 +0300

    created forms for changing one's profile

--- lawyer/forms.py
+++ lawyer/forms.py
@@ -1,7 +1,13 @@
 from django import forms 
-from .models import Articles 
+from .models import Articles
 
 class NewPostForm(forms.ModelForm):
-    model = Articles 
-    exclude = ['time','username']
+    class Meta:
+        model = Articles 
+        exclude = ['time','username']
 
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['time','first_name','email','last_name']
+        
