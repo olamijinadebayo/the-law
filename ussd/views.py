cd0811be31cc4865fd01abdebf87e3e322c6dfb2
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-13 00:17:00 +0300

    testing the view function of sample USSD app

--- ussd/views.py
+++ ussd/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 # Create your views here.
+
+@csrf_exempt
+def index(request):
+    if request.method == 'POST':
+        session_id = request.POST.get('sessionId')
+        service_code = request.POST.get('serviceCode')
+        phone_number = request.POST.get('phoneNumber')
+        text = request.POST.get('text')
+
+        response = ""
+        if text == "":
+            response = "CON What would you want to check \n"
+            response += "1. My Phone Number "
+
+        elif text == "1":
+            response = "END My Phone number is {0} Tolu".format(phone_number)
+
+        return HttpResponse(response)
