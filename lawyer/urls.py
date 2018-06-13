fdbdd1e3558c67c9ee89b585bddfbfb5cd0dfb91
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-13 06:25:04 +0300

    framework for all html

--- lawyer/urls.py
+++ lawyer/urls.py
@@ -5,7 +5,7 @@ app_name='lawyer'
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'$', views.lawyerdashboard, name= 'lawyerdashboard'),
+    url(r'^$', views.lawyerdashboard, name= 'lawyerdashboard'),
     url(r'profile/', views.lawyerprofile, name = 'lawyerprofile'),
     url(r'cases/', views.lawyercases, name = 'lawyercases'),
     url(r'new/article', views.newarticle, name = 'newarticle'),
