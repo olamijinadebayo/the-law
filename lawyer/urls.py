eb72b6466a42e78b9ef9f78c7ba8084d755fa741
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-13 05:48:29 +0300

    changes to the naming of things

--- lawyer/urls.py
+++ lawyer/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url, include
 from .import views 
 
-app_name='lawyers'
+app_name='lawyer'
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'lawyer/$', views.lawyerdashboard, name= 'lawyerdashboard'),
-    url(r'lawyer/profile/', views.lawyerprofile, name = 'lawyerprofile'),
-    url(r'lawyer/cases/', views.lawyercases, name = 'lawyercases'),
-    url(r'lawyer/new/article', views.newarticle, name = 'newarticle'),
-    url(r'lawyer/change_lawyerProfile/(\d+)$', views.change_lawyerProfile, name='change_lawyerProfile'),
+    url(r'$', views.lawyerdashboard, name= 'lawyerdashboard'),
+    url(r'profile/', views.lawyerprofile, name = 'lawyerprofile'),
+    url(r'cases/', views.lawyercases, name = 'lawyercases'),
+    url(r'new/article', views.newarticle, name = 'newarticle'),
+    url(r'change_lawyerProfile/(\d+)$', views.change_lawyerProfile, name='change_lawyerProfile'),
     url(r'^logout/$', views.logout_view, name='logout')
 ]
