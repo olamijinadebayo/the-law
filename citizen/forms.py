019f64801c1f64b85420da44371336447f7ea9d2
muturi254 <petermuturi74@gmail.com>
2018-06-16 02:48:47 +0300

    forms for the citizen app

--- citizen/forms.py
+++ citizen/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Post
+from .models import Post, Citizen
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         exclude = ['citizen']
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Citizen
+        exclude = ['user']
