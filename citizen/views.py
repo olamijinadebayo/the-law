da140ce7525e8db891e72ba64687cf56ad65aeba
muturi254 <petermuturi74@gmail.com>
2018-06-16 02:48:11 +0300

    view for profile edit

--- citizen/views.py
+++ citizen/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect,reverse
 from django.http import HttpResponseRedirect
-from .forms import PostForm
+from django.conf import settings
+from django.db import transaction
+from .forms import PostForm, ProfileForm
 
 # Create your views here.
 def post(request):
@@ -11,9 +13,27 @@ def post(request):
                 post=form.save(commit=False)
                 post.citizen = request.user
                 form.save() 
-                return redirect('accounts:lawyer')
+                return redirect('citizen:edit')
         else:
             form = PostForm()
+            print(request.user)
         return render(request, 'citizen/post.html', {'form': form})
     else:
         return redirect('accounts:citizenSignup')
+
+
+@transaction.atomic
+def profile_edit(request):
+    if request.method == 'POST':
+        profile_form = ProfileForm(
+            request.POST, request.FILES, instance=settings.AUTH_USER_MODEL)
+        if profile_form.is_valid():
+            profile_form.save()
+            # messages.success(request, _(
+            #     'Your profile was successfully updated!'))
+            return redirect('/')
+        else:
+            messages.error(request, ('Please correct the error below.'))
+    else:
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'citizen/edit-profile.html', {"profile_form": profile_form})
