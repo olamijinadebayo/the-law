3e884e42cf09544a2282a702170b87bfd6958ea4
rkemmy <rkemmy.rk69@gmail.com>
2018-06-20 16:25:07 +0300

    register lawyer

--- lawyer/admin.py
+++ lawyer/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 from .models import Lawyer
-admin.site.register(Lawyer)
+from django.contrib.gis import admin as geoAdmin
+from leaflet.admin import LeafletGeoAdmin
+
+
+class LawyerAdmin(geoAdmin.OSMGeoAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone','date')
+    search_fields = ['first_name', 'phone']
+    ordering = ['id']
+    list_filter = ('id', 'phone')
+    default_lon =  -0.561360
+    default_lat = 37.050093
+    default_zoom = 14
+    map_info = True
+    map_width = 700
+    map_height = 500
+
+admin.site.register(Lawyer, LawyerAdmin)
