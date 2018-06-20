ca0d933feaa8de84c15922f4380e5a8f2c491d41
rkemmy <rkemmy.rk69@gmail.com>
2018-06-20 16:22:31 +0300

    add lawyer data forms

--- lawyer/forms.py
+++ lawyer/forms.py
@@ -1,5 +1,8 @@
 from django import forms 
 from .models import Articles, Lawyer
+from django.forms import ModelForm
+from leaflet.forms.widgets import LeafletWidget
+from leaflet.forms.fields import PointField
 
 class NewPostForm(forms.ModelForm):
     class Meta:
@@ -10,4 +13,16 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Lawyer
         exclude = ['time','first_name','email','last_name']
-        
+
+class LawyerForm(forms.ModelForm):
+    location = PointField()
+
+    class Meta:
+        model = Lawyer
+        fields = '__all__'
+
+class DataForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    phone = forms.CharField(max_length=50, required=True)
+    coordinates=forms.CharField(max_length=200, required=True)
