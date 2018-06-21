6440ccdaa666542df0eaf4e2ee18e24bbacc1d0d
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-21 09:42:17 +0300

    changes to the law

--- ussd/urls.py
+++ ussd/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url 
 from . import views 
 
-# app_name = USSD
+app_name = USSD
 urlpatterns = [
-    url(r'^$',views.index, name='index')
+    url(r'^$',views.africastalking, name='africastalking')
+    
 ]
