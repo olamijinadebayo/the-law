0e5b1d027ea3fe0466af2995b6c10a18978561e2
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-23 13:05:49 +0300

    changes to the url used

--- ussd/views.py
+++ ussd/views.py
@@ -7,8 +7,8 @@ import africastalking
 api_key='a98885a5b35185f219cd35263a9a3a7db9196f24a9df7e12a740a9eec53741d4'
 username='sandbox'
 phoneNumber='+254724629599'
-url='cebc73a9.ngrok.io'
-serviceCode='*384*09876#'
+url='https://de-law.herokuapp.com/'
+serviceCode='*384*23519#'
 
 @csrf_exempt
 def index(request):
