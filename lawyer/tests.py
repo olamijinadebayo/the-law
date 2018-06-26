eacb509bf0c11697b82f1a2056e10fcd4647652f
wesleymutwiri <wesleymochiemo@gmail.com>
2018-06-26 09:22:25 +0300

    added new Testcase and README.md

--- lawyer/tests.py
+++ lawyer/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Law, Lawyer, Articles
+
 
 # Create your tests here.
+class LawTestCase(TestCase):
+    '''
+    Class that defines the test suite for the Law Model
+    '''
+    def SetUp(self):
+        '''
+        
